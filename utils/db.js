7459a781f01df42fed173e67b027704afcbec416
Ansahmbom <ansahmbomyong@gmail.com>
2024-06-15 11:23:05 +0300

    db.js

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+// const { promisify } = require('util');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB client error:', err);
+      }
+    });
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
