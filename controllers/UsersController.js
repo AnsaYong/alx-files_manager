6c2c249a4daec962fe4955dbb9d02bb4183acdcd
Ansahmbom <ansahmbomyong@gmail.com>
2024-06-17 19:17:58 +0300

    fixed the listening port in server.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,21 +1,29 @@
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });
 
-    const usersCollection = await dbClient.client.db.collection('users');
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
     const existingUser = await usersCollection.findOne({ email });
 
-    if (existingUser) return res.status(400).json({ error: 'Already exist' });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
 
-    const hasshedPassword = sha1(password);
+    const hashedPassword = sha1(password);
     const newUser = {
       email,
-      password: hasshedPassword,
+      password: hashedPassword,
     };
 
     const result = await usersCollection.insertOne(newUser);
@@ -24,6 +32,32 @@ class UsersController {
     return res.status(201).json({
       id: userId,
       email,
+    });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ _id: new dbClient.ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({
+      id: user._id,
+      email: user.email,
     });
   }
 }
